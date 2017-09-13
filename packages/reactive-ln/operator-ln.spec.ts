69d81019f4732dc9ab684e42568f7443ab8d5ea0
Yishan Wen <wittywys@gmail.com>
2017-09-13 08:59:49 +0800

    learn rx

---
+++ packages/reactive-ln/operator-ln.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      expect(1).eq(1);
+    });
+  });
+});
