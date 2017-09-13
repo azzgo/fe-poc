e59b5b747a2283b243473e7649b6677e9a635e81
Yishan Wen <wittywys@gmail.com>
2017-09-13 09:19:30 +0800

    learn onErrorResumeNext

--- packages/reactive-ln/operator-ln.spec.ts
+++ packages/reactive-ln/operator-ln.spec.ts
@@ -1,10 +1,17 @@
 import { expect } from 'chai'
+import { Observable, EmptyError } from 'rxjs'
 
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      expect(1).eq(1);
-    });
+describe('onErrorResumeNext', () => {
+  it('onErrorResumeNext should return -1', (done) => {
+    Observable.forkJoin([
+      Observable.of(1),
+      Observable.of(2),
+      Observable.throw(new Error('UNKNOWN')),
+      Observable.of(2),
+    ]).onErrorResumeNext(Observable.of(-1))
+    .subscribe((res) => {
+      expect(res).equal(-1)
+      done()
+    })
   });
 });
