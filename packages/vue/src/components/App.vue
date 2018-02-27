aaafc8cf86829866ca03a5593d4bd18a71f2f5ed
Yishan Wen <wittywys@gmail.com>
2018-02-27 15:44:32 +0800

    vue: styles ready

--- packages/vue/src/components/App.vue
+++ packages/vue/src/components/App.vue
@@ -1,10 +1,82 @@
 <template>
-	
-	<div>hello</div>
+  <div>
+    <header class="app-bar row middle-xs">
+      <a href="/" class="logo col-xs-10">
+        Retain
+      </a>
+      <nav class="col-xs-2">
+        <div class="row middle-xs between-xs">
+          <a class="link" to="/app/about">About</a>
+        </div>
+      </nav>
+    </header>
+    <div class="warpper row center-xs">
+      <div class="creator col-xs-6">
+        <new-note-form @onCreateNewNote="createNote"></new-note-form>
+      </div>
+      <div class="notes col-xs-8">
+        <note-list :notes="notes" @onCheckNote="checkNote"></note-list>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script type="text/javascript">
-export default {
+<style type="text/less" scoped>
+  .app-bar {
+    height: 65px;
+    padding: 5px 30px;
+    background-color: #00BCD4;
+  }
+  .logo {
+    color: white;
+    font-size: 30px;
+    font-weight: 300;
+    cursor: pointer;
+  }
+  .link {
+    color: white;
+    font-size: 24px;
+    font-weight: 400;
+    cursor: pointer;
+  }
 
+  .warpper {
+    margin-top: 50px;
+  }
+
+  .notes {
+    padding-top: 50px;
+  }
+
+  .creator {
+    margin-bottom: 40px;
+  }
+</style>
+
+<script type="text/javascript">
+import NewNoteForm from './NewNoteForm'
+import NoteList from './NoteList'
+
+export default {
+  data() {
+    return {
+      notes: []
+    }
+  },
+  methods: {
+    createNote(newNote) {
+      this.notes.push({
+        ...newNote,
+        id: Date.now(),
+      })
+    },
+    checkNote({id}) {
+      this.notes = this.notes.filter((note) => note.id !== id)
+    },
+  },
+  components: {
+    'new-note-form': NewNoteForm,
+    'note-list': NoteList,
+  }
 }
 </script>
---
+++ packages/vue/src/components/NoteList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="row between-xs">
+    <div class="row shadow-1 note-card col-xs-4" v-for="(note, key) in notes" :key="key">
+      <div class="icon" @click="onCheckNote(note)">
+        <i class="material-icons">check</i>
+      </div>
+      <div class="col-xs-12 title">{{note.title}}</div>
+      <div class="col-xs-12 value">{{note.value}}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.note-card {
+  padding: 15px;
+  padding-top: 35px;
+  border-radius: 2px;
+  width: 100%;
+  position: relative;
+}
+
+.title {
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-align: left;
+  color: rgba(0, 0, 0, 0.8);
+}
+
+.value {
+  text-align: left;
+  font-size: 1.4rem;
+  font-weight: 200;
+}
+
+.icon {
+  position: absolute;
+  color: black;
+  border: 1px solid lightgrey;
+  background-color: white;
+  font-size: 30px;
+  top: -10px;
+  left: -10px;
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  cursor: pointer;
+}
+</style>
+
+
+<script>
+export default {
+  props: {
+    notes: Array,
+  },
+  methods: {
+    onCheckNote(note) {
+      this.$emit('onCheckNote', note)
+    }
+  }
+}
+</script>
