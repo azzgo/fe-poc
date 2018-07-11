2b6ad21db443909af3a1b5bae2fdd90465f2ad9c
Yishan Wen <wittywys@gmail.com>
2018-07-11 22:40:11 +0800

    refacotr

--- packages/electron/app/index.js
+++ packages/electron/app/index.js
@@ -25,14 +25,12 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-
   if (win === null) {
     createWindow()
   }
---
+++ packages/electron/renderer/js/app.js
@@ -0,0 +1,7 @@
+new Vue({
+  data() {
+    return {
+      name: 'IShan'
+    }
+  }
+}).$mount('#app')
