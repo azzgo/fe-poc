5ef4c513e0039f7d9dd2560db04a9c58038759c7
Yishan Wen <wittywys@gmail.com>
2019-04-09 16:00:23 +0800

    delete systemjs use modern way !

---
+++ packages/esmodule/js/main.js
@@ -0,0 +1,7 @@
+import React from 'https://unpkg.com/react-ecmascript@1.4.3/react.development.mjs';
+import ReactDOM from 'https://unpkg.com/react-ecmascript@1.4.3/react-dom.development.mjs';
+
+const App = React.createElement('div', null, 'hello')
+
+
+ReactDOM.render(App, document.getElementById('app'))
--- packages/system-lx/js/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const App = React.createElement('div', null, 'hello')
-
-
-ReactDOM.render(App, document.getElementById('app'))
