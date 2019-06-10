a0d95edf497f9d058fe702b110f1e5d985826df5
Yishan Wen <wittywys@gmail.com>
2019-06-10 10:38:04 +0800

    vDOM: 增加列表相关的测试代码

--- packages/vitual-dom-lx/main.js
+++ packages/vitual-dom-lx/main.js
@@ -1,11 +1,12 @@
 require(['render', 'h'], function(render, h) {
-  let vDOM = createVDOM(h, { name: '' });
+  let vDOM = createVDOM(h, { name: '', list: ['这是一条朴实无华的列表项'] });
   render(document.getElementById('app'), vDOM);
 
   function createVDOM(h, state) {
     return h('div', null, [
       h('h1', null, 'Virtual DOM 实现'),
       h('p', null, 'Created By Yishan'),
+      h('h3', null, '输入绑定'),
       h('input', {
         placeholder: '请输入相关值',
         value: state.name,
@@ -17,6 +18,32 @@ require(['render', 'h'], function(render, h) {
         },
       }),
       h('p', null, '你输入的是: ' + state.name),
+      h('h3', null, '列表操作'),
+      ...state.list.map(item => h('p', null, item)),
+      h(
+        'button',
+        {
+          onClick: function(event) {
+            state.list.push('这是一条朴实无华的列表项');
+            let newVDOM = createVDOM(h, state);
+            render(document.getElementById('app'), newVDOM, vDOM);
+            vDOM = newVDOM;
+          },
+        },
+        '+',
+      ),
+      h(
+        'button',
+        {
+          onClick: function(event) {
+            state.list.pop();
+            let newVDOM = createVDOM(h, state);
+            render(document.getElementById('app'), newVDOM, vDOM);
+            vDOM = newVDOM;
+          },
+        },
+        '-',
+      ),
     ]);
   }
 });
