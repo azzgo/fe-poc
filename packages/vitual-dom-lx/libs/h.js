2e7734c677df00c88ee65bfa2a21f6d6b9153eb5
Yishan Wen <wittywys@gmail.com>
2019-07-03 16:58:48 +0800

    add parrentNode

--- packages/vitual-dom-lx/libs/h.js
+++ packages/vitual-dom-lx/libs/h.js
@@ -12,22 +12,31 @@
       this.nodeName = nodeName;
       this.atributes = atributes;
       this.elm = null;
+      this.parentVNode = null;
 
       if (typeof childrens === 'string') {
-        this.text = childrens
-      } else {
+        this.text = childrens;
+      } else if (childrens) {
         if (Array.isArray(childrens)) {
-          this.childrens = childrens.map(this.transFormChildNode)
+          this.childrens = childrens.map(item =>
+            this.transFormChildNode(item, this),
+          );
         } else {
-          this.childrens = [this.transFormChildNode(childrens)]
+          this.childrens = [this.transFormChildNode(childrens, this)];
         }
       }
     }
 
-    transFormChildNode(child) {
-      if (typeof child === 'string')
-        return new VNode(null, null, child);
-      return child;
+    transFormChildNode(child, currentNode) {
+      let newVNode;
+      if (typeof child === 'string') {
+        newVNode = new VNode(null, null, child);
+      } else {
+        newVNode = child;
+      }
+
+      newVNode.parentVNode = currentNode;
+      return newVNode;
     }
   }
 })();
