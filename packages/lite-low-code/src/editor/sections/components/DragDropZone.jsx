5ff8975bc2ad7af237374e3dacb466df568e2766
Ison <azzgo@users.noreply.github.com>
2021-11-18 17:25:10 +0800

    feat: follow vue implement, onStart, onUpdate, onRemove logic

--- packages/lite-low-code/src/editor/sections/components/DragDropZone.jsx
+++ packages/lite-low-code/src/editor/sections/components/DragDropZone.jsx
@@ -1,5 +1,15 @@
+import {cloneDeep} from "lodash";
 import React, {useEffect, useRef, useState} from "react"
 import Sortable from "sortablejs";
+
+function insertNodeAt(fatherNode, node, position) {
+  const refNode =
+    position === 0
+      ? fatherNode.children[0]
+      : fatherNode.children[position - 1].nextSibling;
+  fatherNode.insertBefore(node, refNode);
+}
+
 
 function DragDropZone({render, schema}){
   const dragDropRef = useRef(null);
@@ -11,11 +21,26 @@ function DragDropZone({render, schema}){
       sort: true,
       onAdd(event) {
         event.item.remove();
+        console.log(event.newIndex, 0 , event.item._dragData);
         schema.splice(event.newIndex, 0, event.item._dragData);
         forceUpdate({});
       },
       onUpdate(event) {
-        console.log('??????????????/');
+        event.item.remove();
+        insertNodeAt(event.from, event.item, event.oldIndex);
+        schema.splice(event.newIndex, 0, schema.splice(event.oldIndex, 1)[0]);
+        forceUpdate({});
+      },
+      onRemove(event) {
+        // must keep a dom on origin position then change data。
+        insertNodeAt(dragDropRef.current, event.item, event.oldIndex);
+        schema.splice(event.oldIndex, 1);
+        forceUpdate({});
+      },
+      onStart(event) {
+        const index = Array.prototype.slice.call(dragDropRef.current.childNodes).indexOf(event.item);
+        const _dragSchema = schema[index];
+        event.item._dragData = _dragSchema;
       },
       onMove(event) {
 
